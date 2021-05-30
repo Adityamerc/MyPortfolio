e9d96e34ae8ef3cb35717b5031f163c7d599d68e
Aditya V <mercisthebest98@gmail.com>
2021-05-30 10:57:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from "@material-ui/core/styles";
 
-function App() {
+const mainStyles = makeStyles({
+  main: {
+    backgroundColor: "#01497c",
+    height: "100vh",
+    margin: "0px",
+  },
+  nav: {
+    width: "100vw",
+    position: "fixed",
+    height: "8vh",
+    backgroundColor: "#a9d6e5",
+  },
+});
+
+export default function App() {
+  const styles = mainStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.main}>
+      <nav className={styles.nav}></nav>
     </div>
   );
 }
-
-export default App;
